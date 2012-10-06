6ab0cc9bcb2ed67aa84f72b78411f73ad381e60a
Piotr Murach <pmurach@gmail.com>
2012-10-06 13:33:21 +0100

    Add terminal size detection feature.

---
+++ lib/tty/terminal.rb
@@ -0,0 +1,140 @@
+# -*- encoding: utf-8 -*-
+
+module TTY
+  class Terminal
+
+    @@default_width  = 80
+
+    @@default_height = 24
+
+    # Return default width of terminal
+    #
+    # @example
+    #   default_width = TTY::Terminal.default_width
+    #
+    # @return [Integer]
+    #
+    # @api public
+    def default_width
+      @@default_width
+    end
+
+    # Set default width of terminal
+    #
+    # @param [Integer] width
+    #
+    # @return [Integer]
+    #
+    # @api public
+    def default_width=(width)
+      @@default_width = width
+    end
+
+    # Return default height of terminal
+    #
+    # @example
+    #   default_height = TTY::Terminal.default_height
+    #
+    # @return [Integer]
+    #
+    # @api public
+    def default_height
+      @@default_height
+    end
+
+    # Set default height of terminal
+    #
+    # @example
+    #   default_height = TTY::Terminal.default_height
+    #
+    # @return [Integer]
+    #
+    # @api public
+    def default_height=(height)
+      @@default_height = height
+    end
+
+    # Determine current width
+    #
+    # @return [Integer] width
+    #
+    # @api width
+    def width
+      if ENV['TTY_COLUMNS'] =~ /^\d+$/
+        result = ENV['TTY_COLUMNS'].to_i
+      else
+        result = TTY::System.unix? ? dynamic_width : default_width
+      end
+    rescue
+      default_width
+    end
+
+    # Determine current height
+    #
+    # @api public
+    def height
+      if ENV['TTY_LINES'] =~ /^\d+$/
+        result = ENV['TTY_LINES'].to_i
+      else
+        result = TTY::System.unix? ? dynamic_height : self.default_height
+      end
+    rescue
+      self.default_height
+    end
+
+    # Calculate dynamic width of the terminal
+    #
+    # @return [Integer] width
+    #
+    # @api public
+    def dynamic_width
+      @dynamic_width ||= (dynamic_width_stty.nonzero? || dynamic_width_tput)
+    end
+
+    # Calculate dynamic height of the terminal
+    #
+    # @return [Integer] height
+    #
+    # @api public
+    def dynamic_height
+      @dynamic_height ||= (dynamic_height_stty.nonzero? || dynamic_height_tput)
+    end
+
+    # Detect terminal width with stty utility
+    #
+    # @return [Integer] width
+    #
+    # @api public
+    def dynamic_width_stty
+      %x{stty size 2>/dev/null}.split[1].to_i
+    end
+
+    # Detect terminal height with stty utility
+    #
+    # @return [Integer] height
+    #
+    # @api public
+    def dynamic_height_stty
+      %x{stty size 2>/dev/null}.split[0].to_i
+    end
+
+    # Detect terminal width with tput utility
+    #
+    # @return [Integer] width
+    #
+    # @api public
+    def dynamic_width_tput
+      %x{tput cols 2>/dev/null}.to_i
+    end
+
+    # Detect terminal height with tput utility
+    #
+    # @return [Integer] height
+    #
+    # @api public
+    def dynamic_height_tput
+      %x{tput lines 2>/dev/null}.to_i
+    end
+
+  end # Terminal
+end # TTY
---
+++ spec/tty/terminal/size_spec.rb
@@ -0,0 +1,94 @@
+# -*- encoding: utf-8 -*-
+
+require 'spec_helper'
+
+describe TTY::Terminal, '#size' do
+  let(:default_width)  { 80 }
+  let(:default_height) { 24 }
+
+  it { should be_instance_of(described_class) }
+
+  it { should respond_to(:width) }
+
+  it { should respond_to(:height) }
+
+  its(:default_width) { should eql default_width }
+
+  its(:default_height) { should eql default_height }
+
+  context '#width' do
+    it 'sets the env variable' do
+      ENV.stub(:[]).with('TTY_COLUMNS').and_return '100'
+      subject.width.should eql 100
+    end
+
+    it 'is not unix system' do
+      TTY::System.stub(:unix?) { false }
+      subject.should_receive(:default_width)
+      subject.width
+    end
+
+    it 'is unix system' do
+      TTY::System.stub(:unix?) { true }
+      subject.should_receive(:dynamic_width)
+      subject.width
+    end
+
+    it 'cannot determine width' do
+      ENV.stub(:[]) { raise }
+      subject.should_receive(:default_width)
+      subject.width
+    end
+  end
+
+  context '#height' do
+    it 'sets the env variable' do
+      ENV.stub(:[]).with('TTY_LINES').and_return '50'
+      subject.height.should eql 50
+    end
+
+    it 'is not unix system' do
+      TTY::System.stub(:unix?) { false }
+      subject.should_receive(:default_height)
+      subject.height
+    end
+
+    it 'is unix system' do
+      TTY::System.stub(:unix?) { true }
+      subject.should_receive(:dynamic_height)
+      subject.height
+    end
+
+    it 'cannot determine width' do
+      ENV.stub(:[]) { raise }
+      subject.should_receive(:default_height)
+      subject.height
+    end
+  end
+
+  context '#dynamic_width' do
+    it 'uses stty' do
+      subject.should_receive(:dynamic_width_stty) { 100 }
+      subject.dynamic_width
+    end
+
+    it 'uses tput' do
+      subject.stub(:dynamic_width_stty).and_return 0
+      subject.should_receive(:dynamic_width_tput) { 100 }
+      subject.dynamic_width
+    end
+  end
+
+  context '#dynamic_height' do
+    it 'uses stty' do
+      subject.should_receive(:dynamic_height_stty) { 100 }
+      subject.dynamic_height
+    end
+
+    it 'uses tput' do
+      subject.stub(:dynamic_height_stty).and_return 0
+      subject.should_receive(:dynamic_height_tput) { 100 }
+      subject.dynamic_height
+    end
+  end
+end
