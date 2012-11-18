29f53ad8099b3ab182430db285707ebe04c42e1c
Piotr Murach <pmurach@gmail.com>
2012-11-18 17:54:14 +0000

    Rewrite table initialization spec to include header tests.

--- spec/tty/table/initialize_spec.rb
+++ spec/tty/table/initialize_spec.rb
@@ -12,54 +12,61 @@ describe TTY::Table, '#initialize' do
 
   it { (Enumerable === subject).should be_true }
 
-  it 'initializes header as an option' do
-    table = TTY::Table.new :header => header
-    table.header.should == header
-  end
-
-  it 'initializes with rows as arguments' do
-    table = TTY::Table[*rows]
-    table.to_a.should == rows
-  end
-
-  it 'initializes ' do
-    table = TTY::Table.new rows
-    table.to_a.should == rows
-  end
-
-  it 'initializes table rows as an option' do
-    table = TTY::Table.new :rows => rows
-    table.to_a.should == rows
-  end
-
-  it 'initializes table rows as an argument' do
-    table = TTY::Table.new header, rows
-    table.to_a.should == rows
-  end
-
-  it 'initializes table rows in a block with param' do
-    table = TTY::Table.new  do |t|
-      t << rows[0]
-      t << rows[1]
+  context 'with rows only' do
+    it 'initializes with rows as arguments' do
+      table = TTY::Table[*rows]
+      table.to_a.should == rows
     end
-    table.to_a.should == rows
+
+    it 'initializes with rows' do
+      table = TTY::Table.new rows
+      table.to_a.should == rows
+    end
+
+    it 'initializes table rows as an option' do
+      table = TTY::Table.new :rows => rows
+      table.to_a.should == rows
+    end
+
+    it 'initializes table rows in a block with param' do
+      table = TTY::Table.new  do |t|
+        t << rows[0]
+        t << rows[1]
+      end
+      table.to_a.should == rows
+    end
+
+    it 'initializes table and adds rows' do
+      table = TTY::Table.new
+      table << rows[0]
+      table << rows[1]
+      table.to_a.should == rows
+    end
+
+    it 'chains rows' do
+      table = TTY::Table.new
+      table << rows[0] << rows[1]
+      table.to_a.should == rows
+    end
   end
 
-  it 'initializes table and adds rows' do
-    table = TTY::Table.new
-    table << rows[0]
-    table << rows[1]
-    table.to_a.should == rows
+  context 'with header and rows' do
+    it 'initializes header as an option' do
+      table = TTY::Table.new :header => header
+      table.header.should == header
+    end
+
+    it 'initializes table rows as an argument' do
+      table = TTY::Table.new header, rows
+      table.header.should == header
+      table.to_a.should == rows
+    end
   end
 
-  it 'chains rows' do
-    table = TTY::Table.new
-    table << rows[0] << rows[1]
-    table.to_a.should == rows
-  end
-
-  it 'converts row arguments from hash to array' do
-    table = TTY::Table.new :rows => {:a => 1, :b => 2}
-    table.to_a.should include [:a, 1 ]
+  context 'coercion' do
+    it 'converts row arguments from hash to array' do
+      table = TTY::Table.new :rows => {:a => 1, :b => 2}
+      table.to_a.should include [:a, 1 ]
+    end
   end
 end
