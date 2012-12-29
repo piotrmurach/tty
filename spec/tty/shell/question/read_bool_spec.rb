88d3b9b727de311b0a717adc07dd440db20b7b38
Piotr Murach <pmurach@gmail.com>
2012-12-29 22:41:17 +0000

    Factor out tests for type readers.

---
+++ spec/tty/shell/question/read_bool_spec.rb
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+
+require 'spec_helper'
+
+describe TTY::Shell::Question, '#read_bool' do
+  let(:input)  { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:shell) { TTY::Shell.new(input, output) }
+
+  it 'fails to read boolean' do
+    input << 'invalid'
+    input.rewind
+    q = shell.ask("Do you read books?")
+    expect { q.read_bool }.to raise_error(TTY::TypeError)
+  end
+
+  it 'reads negative boolean' do
+    input << 'No'
+    input.rewind
+    q = shell.ask("Do you read books?")
+    answer = q.read_bool
+    expect(answer).to eql false
+  end
+
+  it 'reads positive boolean' do
+    input << 'Yes'
+    input.rewind
+    q = shell.ask("Do you read books?")
+    answer = q.read_bool
+    expect(answer).to eql true
+  end
+
+  it 'reads single positive boolean' do
+    input << 'y'
+    input.rewind
+    q = shell.ask("Do you read books?")
+    answer = q.read_bool
+    expect(answer).to eql true
+  end
+
+end
---
+++ spec/tty/shell/question/read_string_spec.rb
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+require 'spec_helper'
+
+describe TTY::Shell::Question, '#initialize' do
+  let(:input)  { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:shell) { TTY::Shell.new(input, output) }
+
+  it 'reads string' do
+    name = "Piotr"
+    input << name
+    input.rewind
+    q = shell.ask("What is your name?")
+    answer = q.read_string
+    expect(answer).to be_kind_of String
+    expect(answer).to eql name
+  end
+end
