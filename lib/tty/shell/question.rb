0240da52f8c04208acebb20c4b1a3387be294575
Piotr Murach <pmurach@gmail.com>
2012-12-29 22:39:22 +0000

    Add and fix tests for date reading.

--- lib/tty/shell/question.rb
+++ lib/tty/shell/question.rb
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+require 'date'
+
 module TTY
   class Shell
 
---
+++ spec/tty/shell/question/read_date_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+require 'spec_helper'
+
+describe TTY::Shell::Question, '#read_date' do
+  let(:input)  { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:shell) { TTY::Shell.new(input, output) }
+
+  it 'reads date' do
+    input << "20th April 1887"
+    input.rewind
+    q = shell.ask("When were your born?")
+    answer = q.read_date
+    expect(answer).to be_kind_of Date
+    expect(answer.day).to eql 20
+    expect(answer.month).to eql 4
+    expect(answer.year).to eql 1887
+  end
+end
