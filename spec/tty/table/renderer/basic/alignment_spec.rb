287a62a1aad20d6da58d3e6f1b5630d51a8d772b
Piotr Murach <pmurach@gmail.com>
2013-04-29 22:28:12 +0100

    Change table rows alignment tests.

--- spec/tty/table/renderer/basic/alignment_spec.rb
+++ spec/tty/table/renderer/basic/alignment_spec.rb
@@ -2,30 +2,51 @@
 
 require 'spec_helper'
 
-describe TTY::Table::Renderer::Basic, 'alignment' do
+describe TTY::Table, 'alignment' do
   let(:header) { ['h1', 'h2', 'h3'] }
-  let(:rows) { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
+  let(:rows)   { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
+  let(:options) { { :renderer => :basic, :column_aligns => column_aligns }}
 
-  it 'aligns left by default' do
-    header = ['h1', 'h2']
-    rows = [['aaaaa', 'a'], ['b', 'bbbbb']]
-    table = TTY::Table.new header, rows, :renderer => :basic
-    table.to_s.should == <<-EOS.normalize
-      h1    h2   
-      aaaaa a    
-      b     bbbbb
-    EOS
+  subject(:table) { described_class.new header, rows, options }
+
+  context 'with default' do
+    let(:header) { ['h1', 'h2'] }
+    let(:rows) { [['aaaaa', 'a'], ['b', 'bbbbb']] }
+    let(:column_aligns) { nil }
+
+    it 'aligns left by default' do
+      table.to_s.should == <<-EOS.normalize
+        h1    h2   
+        aaaaa a    
+        b     bbbbb
+      EOS
+    end
   end
 
-  it 'aligns table headers' do
-    header = ['header1', 'head2', 'h3']
-    table = TTY::Table.new header, rows, :render => :basic,
-      :column_aligns => [:left, :center, :right]
-    table.to_s.should == <<-EOS.normalize
-      header1 head2 h3
-      a1       a2   a3
-      b1       b2   b3
-    EOS
+  context 'with different headers' do
+    let(:header) { ['header1', 'head2', 'h3'] }
+    let(:column_aligns) { [:left, :center, :right] }
+
+    it 'aligns headers' do
+      table.to_s.should == <<-EOS.normalize
+        header1 head2 h3
+        a1       a2   a3
+        b1       b2   b3
+      EOS
+    end
+  end
+
+  context 'with different aligns' do
+    let(:header) { nil }
+    let(:rows) { [['aaaaa', 'a'], ['b', 'bbbbb']] }
+    let(:column_aligns)  { [:left, :right] }
+
+    it 'aligns table rows' do
+      table.to_s.should == <<-EOS.normalize
+        aaaaa     a
+        b     bbbbb
+      EOS
+    end
   end
 
   it 'aligns table rows' do
