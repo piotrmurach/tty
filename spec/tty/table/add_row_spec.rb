1d1ec4915ec21c6af36702117cfe000e608ff5ca
Piotr Murach <pmurach@gmail.com>
2013-04-30 23:01:53 +0100

    Test table rows addition.

---
+++ spec/tty/table/add_row_spec.rb
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+require 'spec_helper'
+
+describe TTY::Table, '#<<' do
+  let(:rows) { ['a', 'b', 'c'] }
+  let(:object) { described_class }
+
+  subject(:table) { object[rows] }
+
+  context 'with primitive values' do
+    let(:row) { [1, 2, 3] }
+
+    it 'extracts values correctly' do
+      table << row
+      expect(table.to_a.last).to eql(row)
+    end
+  end
+
+  context 'with complex values' do
+    let(:row) { [1, { :value => 2 }, 3] }
+
+    it 'extracts values correctly' do
+      table << row
+      expect(table.to_a.last).to eql([1,2,3])
+    end
+  end
+end
