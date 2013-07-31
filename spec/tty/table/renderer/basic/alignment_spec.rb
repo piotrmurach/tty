86b82d48be6a3290ee0fcc98a7f37a68afa345bd
Piotr Murach <pmurach@gmail.com>
2013-07-31 23:23:51 +0100

    Tests gardening.

--- spec/tty/table/renderer/basic/alignment_spec.rb
+++ spec/tty/table/renderer/basic/alignment_spec.rb
@@ -3,21 +3,20 @@
 require 'spec_helper'
 
 describe TTY::Table::Renderer::Basic, 'alignment' do
-  let(:header) { ['h1', 'h2', 'h3'] }
-  let(:rows)   { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
-  let(:options) { { :renderer => :basic, :column_aligns => column_aligns }}
-
-  let(:table) { TTY::Table.new(header, rows) }
+  let(:header)  { ['h1', 'h2', 'h3'] }
+  let(:rows)    { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
+  let(:options) { { column_aligns: column_aligns } }
+  let(:table)   { TTY::Table.new(header, rows) }
 
   subject(:renderer) { described_class.new table, options }
 
   context 'with default' do
-    let(:header) { ['h1', 'h2'] }
-    let(:rows)   { [['aaaaa', 'a'], ['b', 'bbbbb']] }
+    let(:header)        { ['h1', 'h2'] }
+    let(:rows)          { [['aaaaa', 'a'], ['b', 'bbbbb']] }
     let(:column_aligns) { nil }
 
     it 'aligns left by default' do
-      renderer.render.should == <<-EOS.normalize
+      expect(renderer.render).to eql <<-EOS.normalize
         h1    h2   
         aaaaa a    
         b     bbbbb
@@ -26,11 +25,11 @@ describe TTY::Table::Renderer::Basic, 'alignment' do
   end
 
   context 'with different headers' do
-    let(:header) { ['header1', 'head2', 'h3'] }
+    let(:header)        { ['header1', 'head2', 'h3'] }
     let(:column_aligns) { [:left, :center, :right] }
 
     it 'aligns headers' do
-      renderer.render.should == <<-EOS.normalize
+      expect(renderer.render).to eql <<-EOS.normalize
         header1 head2 h3
         a1       a2   a3
         b1       b2   b3
@@ -39,12 +38,12 @@ describe TTY::Table::Renderer::Basic, 'alignment' do
   end
 
   context 'with different aligns' do
-    let(:header) { nil }
-    let(:rows) { [['aaaaa', 'a'], ['b', 'bbbbb']] }
+    let(:header)         { nil }
+    let(:rows)           { [['aaaaa', 'a'], ['b', 'bbbbb']] }
     let(:column_aligns)  { [:left, :right] }
 
     it 'aligns table rows' do
-      renderer.render.to_s.should == <<-EOS.normalize
+      expect(renderer.render.to_s).to eql <<-EOS.normalize
         aaaaa     a
         b     bbbbb
       EOS
@@ -52,11 +51,11 @@ describe TTY::Table::Renderer::Basic, 'alignment' do
   end
 
   context 'with individual field aligns' do
-    let(:header) { ['header1', 'header2', 'header3'] }
+    let(:header)        { ['header1', 'header2', 'header3'] }
     let(:column_aligns) { [:left, :center, :right] }
-    let(:options) { { :column_aligns => column_aligns, :renderer => :basic } }
+    let(:options)       { {column_aligns: column_aligns} }
     let(:table) {
-      TTY::Table.new :header => header do |t|
+      TTY::Table.new header: header do |t|
         t << ['a1', 'a2', 'a3']
         t << ['b1', {:value => 'b2', :align => :right}, 'b3']
         t << ['c1', 'c2', {:value => 'c3', :align => :center}]
@@ -74,9 +73,9 @@ describe TTY::Table::Renderer::Basic, 'alignment' do
   end
 
   context 'with aligned header' do
-    let(:rows) { [['aaaaa1', 'a2', 'aaa3'], ['b1', 'bbbb2', 'bb3']] }
-    let(:header) {['h1', {:value => 'h2', :align => :right}, {:value => 'h3', :align => :center}] }
-    let(:options) { { :renderer => :basic } }
+    let(:rows)    { [['aaaaa1', 'a2', 'aaa3'], ['b1', 'bbbb2', 'bb3']] }
+    let(:header)  {['h1', {:value => 'h2', :align => :right}, {:value => 'h3', :align => :center}] }
+    let(:options) { { renderer: :basic } }
 
     it "aligns headres" do
       renderer.render.should == <<-EOS.normalize
