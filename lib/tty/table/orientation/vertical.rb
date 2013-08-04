d95a623c7edacd74727c0b92b35672790ceb2a60
Piotr Murach <pmurach@gmail.com>
2013-08-04 16:06:10 +0100

    Add table vertical slice.

--- lib/tty/table/orientation/vertical.rb
+++ lib/tty/table/orientation/vertical.rb
@@ -6,14 +6,30 @@ module TTY
     # A class representing table orientation
     class Orientation
 
+      # A class responsible for vertical table transformation
       class Vertical < Orientation
 
         def transform(table)
           table.rotate_vertical
         end
 
-      end # Vertical
+        # Slice horizontal table data into vertical
+        #
+        # @param [TTY::Table] table
+        #
+        # @api public
+        def slice(table)
+          header   = table.header
+          row_size = table.row_size
 
+          head = header ? header : (0..row_size).map { |n| (n + 1).to_s }
+
+          (0...row_size).inject([]) do |array, index|
+            array + head.zip(table.rows[index]).map { |row| table.to_row(row) }
+          end
+        end
+
+      end # Vertical
     end # Orientation
   end # Table
 end # TTY
