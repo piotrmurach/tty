5d0244f72575f16b0d5cb1b87620da39ce03ebd6
Piotr Murach <pmurach@gmail.com>
2014-04-18 21:44:05 +0100

    Clean up shell statement.

--- lib/tty/shell/statement.rb
+++ lib/tty/shell/statement.rb
@@ -1,17 +1,22 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 module TTY
+  # A class responsible for shell prompt interactions.
   class Shell
-
     # A class representing a statement output to shell.
     class Statement
-
       # @api private
       attr_reader :shell
       private :shell
 
+      # Flag to display newline
+      #
+      # @api public
       attr_reader :newline
 
+      # Color used to display statement
+      #
+      # @api public
       attr_reader :color
 
       # Initialize a Statement
@@ -27,10 +32,10 @@ module TTY
       #   change the message display to color
       #
       # @api public
-      def initialize(shell=nil, options={})
-        @shell = shell || Shell.new
-        @newline = options.fetch :newline, true
-        @color   = options.fetch :color, nil
+      def initialize(shell = Shell.new, options = {})
+        @shell   = shell
+        @newline = options.fetch(:newline, true)
+        @color   = options.fetch(:color, nil)
       end
 
       # Output the message to the shell
@@ -40,7 +45,7 @@ module TTY
       #
       # @api public
       def declare(message)
-        message = TTY::terminal.color.set message, *color if color
+        message = TTY.terminal.color.set message, *color if color
 
         if newline && /( |\t)(\e\[\d+(;\d+)*m)?\Z/ !~ message
           shell.output.puts message
@@ -49,7 +54,6 @@ module TTY
           shell.output.flush
         end
       end
-
     end # Statement
   end # Shell
 end # TTY
