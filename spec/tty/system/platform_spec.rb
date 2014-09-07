43787e8ac5bf37ad1520ea9ccab012380ae6db90
Piotr Murach <pmurach@gmail.com>
2014-09-07 21:26:36 +0100

    Change system specs to use new syntax.

--- spec/tty/system/platform_spec.rb
+++ spec/tty/system/platform_spec.rb
@@ -1,27 +1,46 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 require 'spec_helper'
 
 describe TTY::System, '#platform' do
-  subject { described_class }
+  subject(:system) { described_class }
 
-  it { should respond_to(:windows?) }
+  it { is_expected.to respond_to(:windows?) }
 
-  it { should respond_to(:unix?) }
+  it { is_expected.to respond_to(:unix?) }
 
-  it { should respond_to(:which) }
+  it { is_expected.to respond_to(:which) }
 
-  it { should respond_to(:exists?) }
+  it { is_expected.to respond_to(:exists?) }
 
-  it { should respond_to(:editor) }
+  it { is_expected.to respond_to(:editor) }
 
-  it 'checks if windows' do
-    RbConfig::CONFIG.stub(:[]).with('host_os').and_return 'windows'
-    subject.windows?.should be_true
+  context 'windows?' do
+    it 'is on windows' do
+      allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return('windows')
+      expect(system.windows?).to eq(true)
+    end
+
+    it 'is on windows ignoring case' do
+      allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return('Windows')
+      expect(system.windows?).to eq(true)
+    end
+
+    it 'is not on windows' do
+      allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return('unknown')
+      expect(system.windows?).to eq(false)
+    end
   end
 
-  it 'checks if unix' do
-    RbConfig::CONFIG.stub(:[]).with('host_os').and_return 'darwin'
-    subject.unix?.should be_true
+  context 'unix?' do
+    it 'checks if unix' do
+      allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return('darwin')
+      expect(system.unix?).to eq(true)
+    end
+
+    it 'checks if unix' do
+      allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return('unknown')
+      expect(system.unix?).to eq(false)
+    end
   end
 end
--- spec/tty/system/which_spec.rb
+++ spec/tty/system/which_spec.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 require 'spec_helper'
 
@@ -6,10 +6,10 @@ describe TTY::System, '#which' do
   let(:command) { 'ruby' }
   let(:which)   { double(:which, :which => command) }
 
-  subject { described_class }
+  subject(:system) { described_class }
 
   it 'seeks system command' do
-    TTY::System::Which.should_receive(:new).with(command).and_return(which)
-    expect(subject.which(command)).to eql(command)
+    allow(TTY::System::Which).to receive(:new).with(command).and_return(which)
+    expect(system.which(command)).to eql(command)
   end
 end
