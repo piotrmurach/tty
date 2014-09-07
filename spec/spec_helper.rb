b6e40415d94d3cabe16f97b93da26a5e0aefbc3f
Piotr Murach <pmurach@gmail.com>
2014-09-07 14:52:30 +0100

    Raise errors on deprecations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ end
 require 'tty'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+  config.raise_errors_for_deprecations!
 end
 
 class String
