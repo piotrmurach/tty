281c394fba16103a49a65ae0d135e1a7eebdeb59
Piotr Murach <pmurach@gmail.com>
2014-09-07 19:56:12 +0100

    Change basic terminal pager specs to use new syntax.

--- spec/tty/terminal/pager/basic/page_spec.rb
+++ spec/tty/terminal/pager/basic/page_spec.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 require 'spec_helper'
 
@@ -9,18 +9,18 @@ describe TTY::Terminal::BasicPager, '#simple' do
   let(:terminal) { TTY.terminal }
   let(:object)   { described_class }
 
-  subject { object.new(text) }
+  subject(:pager) { object.new(text) }
 
   before {
-    TTY.stub(:shell).and_return(shell)
-    terminal.stub(:height).and_return(10)
+    allow(TTY).to receive(:shell).and_return(shell)
+    allow(terminal).to receive(:height).and_return(10)
   }
 
   context 'when no text' do
     let(:text) { "" }
 
     it "doesn't page text" do
-      subject.page
+      pager.page
       expect(output.string).to eql(text)
     end
   end
@@ -29,7 +29,7 @@ describe TTY::Terminal::BasicPager, '#simple' do
     let(:text) { "a\n" }
 
     it "doesn't page text not long enough" do
-      subject.page
+      pager.page
       expect(output.string).to eql(text)
     end
   end
@@ -40,14 +40,14 @@ describe TTY::Terminal::BasicPager, '#simple' do
     it "continues paging when enter is pressed" do
       input << '\n'
       input.rewind
-      subject.page
+      pager.page
       expect(output.string).to eql("a\na\na\na\na\na\na\n\n#{object::PAGE_BREAK}\na\na\na\n")
     end
 
     it "stops paging when q is pressed" do
       input << 'q\n'
       input.rewind
-      subject.page
+      pager.page
       expect(output.string).to eql("a\na\na\na\na\na\na\n\n#{object::PAGE_BREAK}\n")
     end
   end
