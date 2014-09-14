415347c6901937c54fdc0e202c4a4a7b8bcf4f94
Piotr Murach <pmurach@gmail.com>
2014-09-14 18:53:02 +0100

    Update specs to latest version.

--- spec/tty/table/column_set/total_width_spec.rb
+++ spec/tty/table/column_set/total_width_spec.rb
@@ -1,10 +1,10 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 require 'spec_helper'
 
 describe TTY::Table::ColumnSet, '#extract_widths!' do
   let(:header) { ['h1', 'h2', 'h3'] }
-  let(:rows) { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
+  let(:rows)   { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
   let(:table)  { TTY::Table.new header, rows }
 
   subject { described_class.new table }
