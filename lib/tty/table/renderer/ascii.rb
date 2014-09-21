7011ccf7e6afca83091cf7ed725a869fcd26c2a4
Piotr Murach <pmurach@gmail.com>
2014-09-21 13:37:17 +0100

    Clean up table ascii renderer.

--- lib/tty/table/renderer/ascii.rb
+++ lib/tty/table/renderer/ascii.rb
@@ -1,14 +1,15 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 module TTY
   class Table
     class Renderer
       class ASCII < Basic
-
-        def initialize(table, options={})
-          super(table, options.merge(:border_class => TTY::Table::Border::ASCII))
+        # Create ASCII renderer
+        #
+        # @api private
+        def initialize(table, options = {})
+          super(table, options.merge(border_class: TTY::Table::Border::ASCII))
         end
-
       end # ASCII
     end # Renderer
   end # Table
