8d71481b6ef6ef356b682ca5a54d6b054fbc4569
Piotr Murach <pmurach@gmail.com>
2014-09-21 17:30:20 +0100

    Clarify intent.

--- spec/tty/logger/new_spec.rb
+++ spec/tty/logger/new_spec.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 require 'spec_helper'
 
@@ -6,16 +6,16 @@ describe TTY::Logger, '#new' do
   let(:object) { described_class }
   let(:output) { StringIO.new }
 
-  subject { object.new options }
+  subject(:logger) { object.new(options) }
 
   context 'when default' do
     let(:options) { {namespace: ''} }
 
-    it { expect(subject.level).to eq(object::ALL) }
+    it { expect(logger.level).to eq(object::ALL) }
 
-    it { expect(subject.output).to eq($stderr) }
+    it { expect(logger.output).to eq($stderr) }
 
-    it { expect(subject.timestamp_format).to eq('%Y-%m-%d %T') }
+    it { expect(logger.timestamp_format).to eq('%Y-%m-%d %T') }
   end
 
   context 'when custom' do
@@ -25,12 +25,12 @@ describe TTY::Logger, '#new' do
       output: output,
       timestamp_format: "%dd" } }
 
-    it { expect(subject.namespace).to eq('tty::color') }
+    it { expect(logger.namespace).to eq('tty::color') }
 
-    it { expect(subject.level).to eq(2) }
+    it { expect(logger.level).to eq(2) }
 
-    it { expect(subject.output).to eq(output) }
+    it { expect(logger.output).to eq(output) }
 
-    it { expect(subject.timestamp_format).to eq('%dd') }
+    it { expect(logger.timestamp_format).to eq('%dd') }
   end
 end
