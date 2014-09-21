9b0f09fca19029fecc8e7133e6b30080786a0a9c
Piotr Murach <pmurach@gmail.com>
2014-09-21 13:48:12 +0100

    Clean up table unicode renderer.

--- lib/tty/table/renderer/unicode.rb
+++ lib/tty/table/renderer/unicode.rb
@@ -1,14 +1,20 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 module TTY
   class Table
     class Renderer
+      # Unicode representation of table renderer
+      #
+      # @api private
       class Unicode < Basic
-
-        def initialize(table, options={})
-          super(table, options.merge(:border_class => TTY::Table::Border::Unicode))
+        # Create Unicode renderer
+        #
+        # @param [Table] table
+        #
+        # @api private
+        def initialize(table, options = {})
+          super(table, options.merge(border_class: TTY::Table::Border::Unicode))
         end
-
       end # Unicode
     end # Renderer
   end # Table
