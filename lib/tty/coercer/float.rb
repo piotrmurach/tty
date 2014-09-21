04dde7a2b9821d716c4b50e9aa201ae9ab9605e6
Piotr Murach <pmurach@gmail.com>
2014-09-21 14:59:51 +0100

    Clean up float coercer and update specs.

--- lib/tty/coercer/float.rb
+++ lib/tty/coercer/float.rb
@@ -1,23 +1,20 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 module TTY
   class Coercer
-
-      class Float
-
-        def self.coerce(value, strict=true)
-          begin
-            Kernel.send(:Float, value.to_s)
-          rescue
-            if strict
-              raise InvalidArgument, "#{value} could not be coerced into Float"
-            else
-              value.to_f
-            end
-          end
+    # Coerce values into float number
+    #
+    # @api public
+    class Float
+      def self.coerce(value, strict = true)
+        Kernel.send(:Float, value.to_s)
+      rescue
+        if strict
+          raise InvalidArgument, "#{value} could not be coerced into Float"
+        else
+          value.to_f
         end
-
-      end # Float
-
+      end
+    end # Float
   end # Coercer
 end # TTY
--- spec/tty/coercer/float/coerce_spec.rb
+++ spec/tty/coercer/float/coerce_spec.rb
@@ -1,7 +1,10 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe TTY::Coercer::Float, '#coerce' do
   let(:strict) { true }
+
   subject { described_class.coerce(value, strict) }
 
   context 'with empty' do
@@ -13,7 +16,7 @@ describe TTY::Coercer::Float, '#coerce' do
   context 'with 1 as string' do
     let(:value) { '1' }
 
-    it { expect(subject).to eql 1.0 }
+    it { is_expected.to eql 1.0 }
   end
 
   context 'with float' do
@@ -23,10 +26,9 @@ describe TTY::Coercer::Float, '#coerce' do
   end
 
   context 'with float not strict' do
-    let(:value) { '1.2abc'}
+    let(:value)  { '1.2abc'}
     let(:strict) { false }
 
-    it { expect(subject).to eql 1.2 }
+    it { is_expected.to eql 1.2 }
   end
-
 end
