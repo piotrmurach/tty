851aca7f83efd9d043170b3384677611972008df
Piotr Murach <pmurach@gmail.com>
2014-09-21 14:49:17 +0100

    Update boolean coercer specs.

--- spec/tty/coercer/boolean/coerce_spec.rb
+++ spec/tty/coercer/boolean/coerce_spec.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe TTY::Coercer::Boolean, '#coerce' do
+
   subject { described_class.coerce(value) }
 
   context "with empty" do
@@ -12,97 +15,97 @@ describe TTY::Coercer::Boolean, '#coerce' do
   context "with true" do
     let(:value) { true }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with 'true'" do
     let(:value) { 'true' }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with TRUE" do
     let(:value) { TRUE }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with 'TRUE'" do
     let(:value) { 'TRUE' }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with 't'" do
     let(:value) { 't' }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with 'T'" do
     let(:value) { 'T' }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with 1" do
     let(:value) { 1 }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with '1'" do
     let(:value) { '1' }
 
-    it { expect(subject).to be_true }
+    it { expect(subject).to eq(true) }
   end
 
   context "with false" do
     let(:value) { false }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with 'false'" do
     let(:value) { 'false' }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with FALSE" do
     let(:value) { FALSE }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with 'FALSE'" do
     let(:value) { 'FALSE' }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with 'f'" do
     let(:value) { 'f' }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with 'F'" do
     let(:value) { 'F' }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with 0" do
     let(:value) { 0 }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with '0'" do
     let(:value) { '0' }
 
-    it { expect(subject).to be_false }
+    it { expect(subject).to eq(false) }
   end
 
   context "with FOO" do
