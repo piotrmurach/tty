3dc3ce385581d32068e68ecfc023df69c7cc82b4
Piotr Murach <pmurach@gmail.com>
2014-09-21 14:55:11 +0100

    Update range coercer specs.

--- spec/tty/coercer/boolean/coerce_spec.rb
+++ spec/tty/coercer/boolean/coerce_spec.rb
@@ -15,97 +15,97 @@ describe TTY::Coercer::Boolean, '#coerce' do
   context "with true" do
     let(:value) { true }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with 'true'" do
     let(:value) { 'true' }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with TRUE" do
     let(:value) { TRUE }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with 'TRUE'" do
     let(:value) { 'TRUE' }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with 't'" do
     let(:value) { 't' }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with 'T'" do
     let(:value) { 'T' }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with 1" do
     let(:value) { 1 }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with '1'" do
     let(:value) { '1' }
 
-    it { expect(subject).to eq(true) }
+    it { is_expected.to eq(true) }
   end
 
   context "with false" do
     let(:value) { false }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with 'false'" do
     let(:value) { 'false' }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with FALSE" do
     let(:value) { FALSE }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with 'FALSE'" do
     let(:value) { 'FALSE' }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with 'f'" do
     let(:value) { 'f' }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with 'F'" do
     let(:value) { 'F' }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with 0" do
     let(:value) { 0 }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with '0'" do
     let(:value) { '0' }
 
-    it { expect(subject).to eq(false) }
+    it { is_expected.to eq(false) }
   end
 
   context "with FOO" do
--- spec/tty/coercer/range/coerce_spec.rb
+++ spec/tty/coercer/range/coerce_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe TTY::Coercer::Range, '#coerce' do
@@ -13,61 +15,60 @@ describe TTY::Coercer::Range, '#coerce' do
   context 'with 1' do
     let(:value) { '1' }
 
-    it { should == (1..1) }
+    it { is_expected.to eq(1..1) }
   end
 
   context 'with 1..10' do
     let(:value) { '1..10' }
 
-    it { should == (1..10) }
+    it { is_expected.to eq(1..10) }
   end
 
   context 'with 1-10' do
     let(:value) { '1-10' }
 
-    it { should == (1..10) }
+    it { is_expected.to eq(1..10) }
   end
 
   context 'with 1,10' do
     let(:value) { '1,10' }
 
-    it { should == (1..10) }
+    it { is_expected.to eq(1..10) }
   end
 
   context 'with 1...10' do
     let(:value) { '1...10'}
 
-    it { should == (1...10) }
+    it { is_expected.to eq(1...10) }
   end
 
   context 'with -1..10' do
     let(:value) { '-1..10' }
 
-    it { should == (-1..10) }
+    it { is_expected.to eq(-1..10) }
   end
 
   context 'with 1..-10' do
     let(:value) { '1..-10' }
 
-    it { should == (1..-10) }
+    it { is_expected.to eq(1..-10) }
   end
 
   context 'with a..z' do
     let(:value) { 'a..z' }
 
-    it { should == ('a'..'z')}
+    it { is_expected.to eq('a'..'z')}
   end
 
   context 'with a-z' do
     let(:value) { 'a-z' }
 
-    it { should == ('a'..'z')}
+    it { is_expected.to eq('a'..'z')}
   end
 
   context 'with A..Z' do
     let(:value) { 'A..Z' }
 
-    it { should == ('A'..'Z')}
+    it { is_expected.to eq('A'..'Z')}
   end
-
 end # coerce
