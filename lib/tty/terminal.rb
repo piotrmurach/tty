00204336a933dd955f9a078220ee765bd7a4a927
Piotr Murach <pmurach@gmail.com>
2015-11-01 18:30:01 +0000

    Remove terminal mode.

--- lib/tty/terminal.rb
+++ lib/tty/terminal.rb
@@ -14,55 +14,7 @@ module TTY
     # @api public
     def initialize(options = {})
       @color = Pastel.new
-      @echo  = TTY::Terminal::Echo.new
-      @raw   = TTY::Terminal::Raw.new
       @home  = Home.new
-    end
-
-    # Switch echo on
-    #
-    # @api public
-    def echo_on
-      @echo.on
-    end
-
-    # Switch echo off
-    #
-    # @api public
-    def echo_off
-      @echo.off
-    end
-
-    # Echo given block
-    #
-    # @param [Boolean] is_on
-    #
-    # @api public
-    def echo(is_on = true, &block)
-      @echo.echo(is_on, &block)
-    end
-
-    # Switch raw mode on
-    #
-    # @api public
-    def raw_on
-      @raw.on
-    end
-
-    # Switch raw mode off
-    #
-    # @api public
-    def raw_off
-      @raw.off
-    end
-
-    # Use raw mode in the given block
-    #
-    # @param [Boolean] is_on
-    #
-    # @api public
-    def raw(is_on = true, &block)
-      @raw.raw(is_on, &block)
     end
 
     # Find user home directory
