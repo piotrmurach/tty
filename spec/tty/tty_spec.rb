76109f5b7f1d62954241fd6890f2551410f0cf74
Piotr Murach <pmurach@gmail.com>
2016-05-30 16:24:24 +0100

    Fix specs.

--- spec/tty/tty_spec.rb
+++ spec/tty/tty_spec.rb
@@ -7,10 +7,6 @@ describe TTY do
 
   it { expect(object.terminal).to be_instance_of(TTY::Terminal) }
 
-  #it { expect(object.shell).to be_instance_of(TTY::Shell) }
-
-  it { expect(object.system).to be(TTY::System) }
-
   it { expect(object.plugins).to be_instance_of(TTY::Plugins) }
 
   context 'when module' do
