e2de89b0fe9ef9a4773e8a98a6d6cb23396098f7
Piotr Murach <pmurach@gmail.com>
2017-01-15 15:07:20 +0000

    Change to use plugins loading mechanism.

--- lib/tty.rb
+++ lib/tty.rb
@@ -1,29 +1,10 @@
 # encoding: utf-8
 
-require 'equatable'
-require 'pastel'
-require 'tty-color'
-require 'tty-cursor'
-require 'tty-command'
-require 'tty-editor'
-#require 'tty-file'
-require 'tty-pager'
-require 'tty-platform'
-require 'tty-progressbar'
-require 'tty-prompt'
-require 'tty-screen'
-require 'tty-spinner'
-#require 'tty-table'
-require 'tty-which'
-
 require 'tty/version'
 require 'tty/plugins'
 require 'tty/plugins/plugin'
 
 module TTY
-  # An empty array used as a default value
-  EMPTY_ARRAY = Array.new.freeze
-
   class << self
     def included(base)
       base.send :extend, ClassMethods
@@ -43,3 +24,5 @@ module TTY
 
   extend ClassMethods
 end # TTY
+
+TTY.plugins.find('tty').load
