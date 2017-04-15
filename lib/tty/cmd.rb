66b2b76af78be3a5c311aff53ce194f29b174d15
Piotr Murach <pmurach@gmail.com>
2017-04-15 23:31:55 +0100

    Change to add ability to run external commands.

--- lib/tty/cmd.rb
+++ lib/tty/cmd.rb
@@ -1,15 +1,23 @@
 # encoding: utf-8
 
-require_relative 'sh'
+require 'forwardable'
+require 'tty-command'
 
 module TTY
   class Cmd
+    extend Forwardable
+
+    def_delegators :command, :run
 
     def execute(*)
       raise(
         NotImplementedError,
         "#{self.class}##{__method__} must be implemented"
       )
+    end
+
+    def command
+      @command ||= TTY::Command.new(printer: :null)
     end
   end # Cmd
 end # TTY
