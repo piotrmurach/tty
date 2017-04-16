6af271b739067b7126a1a648da4d584cb50e05b3
Piotr Murach <pmurach@gmail.com>
2017-04-16 19:08:48 +0100

    Add test for new license generation.

--- spec/integration/new_license_spec.rb
+++ spec/integration/new_license_spec.rb
@@ -1,16 +1,21 @@
 # encoding: utf-8
 
 RSpec.describe 'rtty new --license', type: :cli do
-  it "generates a license file" do
+  it "generates a MIT license file" do
     app_name = tmp_path('newcli')
     command = "bundle exec rtty new #{app_name} --license mit"
     `#{command}`
     license = File.binread(tmp_path('newcli/LICENSE.txt'))
 
-    expect(license.lines[0..2]).to eq([
-      "The MIT License (MIT)\n",
-      "\n",
-      "Copyright (c) 2017 Piotr Murach\n"
-    ])
+    expect(license.lines[0]).to eq("The MIT License (MIT)\n")
+  end
+
+  it "generates a GPL-3.0 license file" do
+    app_name = tmp_path('newcli')
+    command = "bundle exec rtty new #{app_name} -l gplv3"
+    `#{command}`
+    license = File.binread(tmp_path('newcli/LICENSE.txt'))
+
+    expect(license.lines[0]).to eq("GNU GENERAL PUBLIC LICENSE\n")
   end
 end
