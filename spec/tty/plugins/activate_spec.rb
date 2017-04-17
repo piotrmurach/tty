813e792ea6c5cb8aff7576447a3c5e12799e91a2
Piotr Murach <pmurach@gmail.com>
2017-04-17 23:09:27 +0100

    Change plugins loading to allow specifying gemspec path.

--- spec/tty/plugins/activate_spec.rb
+++ spec/tty/plugins/activate_spec.rb
@@ -1,14 +1,12 @@
 # encoding: utf-8
 
-require 'spec_helper'
-
-RSpec.describe TTY::Plugins, '#load' do
+RSpec.describe TTY::Plugins, '#activate' do
   it "activates all plugins" do
     plugin = double(:plugin, :enabled? => false, :load! => true)
     plugins = TTY::Plugins.new
     allow(plugins).to receive(:plugins).and_return([plugin, plugin])
 
-    plugins.load
+    plugins.activate
 
     expect(plugin).to have_received(:load!).twice
   end
---
+++ spec/tty/plugins/load_from_spec.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+RSpec.describe TTY::Plugins, '#load_from' do
+  it "loads gems with a specific pattern" do
+    plugins = TTY::Plugins.new
+    gemspec_path = fixtures_path('foo-0.0.1.gemspec')
+    plugins.load_from(gemspec_path, /^tty-(.*)/)
+
+    expect(plugins.to_a.map(&:name)).to eq([
+      'tty-command',
+      'tty-prompt',
+      'tty-spinner'
+    ])
+  end
+
+  it "allows for complex pattern" do
+    plugins = TTY::Plugins.new
+    gemspec_path = fixtures_path('foo-0.0.1.gemspec')
+    plugins.load_from(gemspec_path, /^tty-(.*)|pastel/)
+
+    expect(plugins.to_a.map(&:name)).to eq([
+      'pastel',
+      'tty-command',
+      'tty-prompt',
+      'tty-spinner'
+    ])
+  end
+end
