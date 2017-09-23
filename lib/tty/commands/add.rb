96a703c8dd236b64b28d2dae7802de6444f13e5d
Piotr Murach <pmurach@gmail.com>
2017-09-23 18:17:20 +0100

    Add placeholder for add command

---
+++ lib/tty/commands/add.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative '../cmd'
+
+module TTY
+  module Commands
+    class Add < TTY::Cmd
+      def initialize(name, options)
+      end
+
+      def execute
+      end
+    end # Add
+  end # Commands
+end # TTY
