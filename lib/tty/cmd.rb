d402528af3dbae328c756a7eb9640715d5bcef69
Piotr Murach <pmurach@gmail.com>
2017-09-23 19:52:45 +0100

    Change to use path helpers

--- lib/tty/cmd.rb
+++ lib/tty/cmd.rb
@@ -7,44 +7,25 @@ require 'tty-prompt'
 require 'tty-which'
 require 'tty-file'
 
+require_relative 'path_helpers'
+
 module TTY
   class Cmd
     extend Forwardable
+    include PathHelpers
 
-    GEMSPEC_PATH = Pathname(__dir__).join("../../tty.gemspec").realpath.to_s
+    GEMSPEC_PATH = Pathname(__dir__).join('../../tty.gemspec').realpath.to_s
 
     def_delegators :command, :run
 
     def_delegators :generator, :copy_file, :inject_into_file, :replace_in_file
 
+    # @api public
     def execute(*)
       raise(
         NotImplementedError,
         "#{self.class}##{__method__} must be implemented"
       )
-    end
-
-    # The root path for all the templates
-    #
-    # @api public
-    def templates_root_path
-      Pathname.new(::File.join(::File.dirname(__FILE__), 'templates'))
-    end
-
-    # The root path of the app running this command
-    #
-    # @return [Pathname]
-    #
-    # @api public
-    def root_path
-      @root_path ||= Pathname.pwd
-    end
-
-    # Execute command within root path
-    #
-    # @api public
-    def within_root_path(&block)
-      Dir.chdir(root_path, &block)
     end
 
     # @api public
