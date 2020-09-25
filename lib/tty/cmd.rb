a2902e2dadb641c4fead90ea0a6cc72e10f248fd
Piotr Murach <pmurach@gmail.com>
2020-09-25 23:15:06 +0100

    Remove unused helper methods from command

--- lib/tty/cmd.rb
+++ lib/tty/cmd.rb
@@ -63,26 +63,6 @@ module TTY
       TTY::Command.new(**options)
     end
 
-    # The cursor movement
-    #
-    # @see http://www.rubydoc.info/gems/tty-cursor
-    #
-    # @api public
-    def cursor
-      require "tty-cursor"
-      TTY::Cursor
-    end
-
-    # Open a file or text in the user's preferred editor
-    #
-    # @see http://www.rubydoc.info/gems/tty-editor
-    #
-    # @api public
-    def editor
-      require "tty-editor"
-      TTY::Editor
-    end
-
     # File manipulation utility methods
     #
     # @see http://www.rubydoc.info/gems/tty-file
@@ -91,56 +71,6 @@ module TTY
     def generator
       require "tty-file"
       TTY::File
-    end
-
-    # Terminal output paging
-    #
-    # @see http://www.rubydoc.info/gems/tty-pager
-    #
-    # @api public
-    def pager(**options)
-      require "tty-pager"
-      TTY::Pager.new(options)
-    end
-
-    # Terminal platform and OS properties
-    #
-    # @see http://www.rubydoc.info/gems/tty-pager
-    #
-    # @api public
-    def platform
-      require "tty-platform"
-      TTY::Platform.new
-    end
-
-    # The interactive prompt
-    #
-    # @see http://www.rubydoc.info/gems/tty-prompt
-    #
-    # @api public
-    def prompt(**options)
-      require "tty-prompt"
-      TTY::Prompt.new(options)
-    end
-
-    # Get terminal screen properties
-    #
-    # @see http://www.rubydoc.info/gems/tty-screen
-    #
-    # @api public
-    def screen
-      require "tty-screen"
-      TTY::Screen
-    end
-
-    # The unix which utility
-    #
-    # @see http://www.rubydoc.info/gems/tty-which
-    #
-    # @api public
-    def which(*args)
-      require "tty-which"
-      TTY::Which.which(*args)
     end
 
     # Check if executable exists
