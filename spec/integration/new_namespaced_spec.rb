983ced316bac3b8e38c5f718114374ea2d11e308
Piotr Murach <pmurach@gmail.com>
2020-09-29 23:44:14 +0100

    Change command template to only show two commented out examples of TTY components usage

--- spec/integration/new_namespaced_spec.rb
+++ spec/integration/new_namespaced_spec.rb
@@ -108,15 +108,9 @@ end
       expect(::File.read("lib/cli/app/command.rb")).to eq(<<-EOS)
 # frozen_string_literal: true
 
-require 'forwardable'
-
 module Cli
   module App
     class Command
-      extend Forwardable
-
-      def_delegators :command, :run
-
       # Execute this command
       #
       # @api public
@@ -127,105 +121,27 @@ module Cli
         )
       end
 
+      # Below are examples of how to integrate TTY components
+
       # The external commands runner
       #
       # @see http://www.rubydoc.info/gems/tty-command
       #
       # @api public
-      def command(**options)
-        require 'tty-command'
-        TTY::Command.new(options)
-      end
-
-      # The cursor movement
-      #
-      # @see http://www.rubydoc.info/gems/tty-cursor
-      #
-      # @api public
-      def cursor
-        require 'tty-cursor'
-        TTY::Cursor
-      end
-
-      # Open a file or text in the user's preferred editor
-      #
-      # @see http://www.rubydoc.info/gems/tty-editor
-      #
-      # @api public
-      def editor
-        require 'tty-editor'
-        TTY::Editor
-      end
-
-      # File manipulation utility methods
-      #
-      # @see http://www.rubydoc.info/gems/tty-file
-      #
-      # @api public
-      def generator
-        require 'tty-file'
-        TTY::File
-      end
-
-      # Terminal output paging
-      #
-      # @see http://www.rubydoc.info/gems/tty-pager
-      #
-      # @api public
-      def pager(**options)
-        require 'tty-pager'
-        TTY::Pager.new(options)
-      end
-
-      # Terminal platform and OS properties
-      #
-      # @see http://www.rubydoc.info/gems/tty-platform
-      #
-      # @api public
-      def platform
-        require 'tty-platform'
-        TTY::Platform.new
-      end
+      # def command(**options)
+      #   require "tty-command"
+      #   TTY::Command.new(options)
+      # end
 
       # The interactive prompt
       #
       # @see http://www.rubydoc.info/gems/tty-prompt
       #
       # @api public
-      def prompt(**options)
-        require 'tty-prompt'
-        TTY::Prompt.new(options)
-      end
-
-      # Get terminal screen properties
-      #
-      # @see http://www.rubydoc.info/gems/tty-screen
-      #
-      # @api public
-      def screen
-        require 'tty-screen'
-        TTY::Screen
-      end
-
-      # The unix which utility
-      #
-      # @see http://www.rubydoc.info/gems/tty-which
-      #
-      # @api public
-      def which(*args)
-        require 'tty-which'
-        TTY::Which.which(*args)
-      end
-
-      # Check if executable exists
-      #
-      # @see http://www.rubydoc.info/gems/tty-which
-      #
-      # @api public
-      def exec_exist?(*args)
-        require 'tty-which'
-        TTY::Which.exist?(*args)
-      end
+      # def prompt(**options)
+      #   require "tty-prompt"
+      #   TTY::Prompt.new(options)
+      # end
     end
   end
 end
--- spec/integration/new_spec.rb
+++ spec/integration/new_spec.rb
@@ -185,14 +185,8 @@ end
       expect(::File.read("lib/newcli/command.rb")).to eq(<<-EOS)
 # frozen_string_literal: true
 
-require 'forwardable'
-
 module Newcli
   class Command
-    extend Forwardable
-
-    def_delegators :command, :run
-
     # Execute this command
     #
     # @api public
@@ -203,105 +197,27 @@ module Newcli
       )
     end
 
+    # Below are examples of how to integrate TTY components
+
     # The external commands runner
     #
     # @see http://www.rubydoc.info/gems/tty-command
     #
     # @api public
-    def command(**options)
-      require 'tty-command'
-      TTY::Command.new(options)
-    end
-
-    # The cursor movement
-    #
-    # @see http://www.rubydoc.info/gems/tty-cursor
-    #
-    # @api public
-    def cursor
-      require 'tty-cursor'
-      TTY::Cursor
-    end
-
-    # Open a file or text in the user's preferred editor
-    #
-    # @see http://www.rubydoc.info/gems/tty-editor
-    #
-    # @api public
-    def editor
-      require 'tty-editor'
-      TTY::Editor
-    end
-
-    # File manipulation utility methods
-    #
-    # @see http://www.rubydoc.info/gems/tty-file
-    #
-    # @api public
-    def generator
-      require 'tty-file'
-      TTY::File
-    end
-
-    # Terminal output paging
-    #
-    # @see http://www.rubydoc.info/gems/tty-pager
-    #
-    # @api public
-    def pager(**options)
-      require 'tty-pager'
-      TTY::Pager.new(options)
-    end
-
-    # Terminal platform and OS properties
-    #
-    # @see http://www.rubydoc.info/gems/tty-platform
-    #
-    # @api public
-    def platform
-      require 'tty-platform'
-      TTY::Platform.new
-    end
+    # def command(**options)
+    #   require "tty-command"
+    #   TTY::Command.new(options)
+    # end
 
     # The interactive prompt
     #
     # @see http://www.rubydoc.info/gems/tty-prompt
     #
     # @api public
-    def prompt(**options)
-      require 'tty-prompt'
-      TTY::Prompt.new(options)
-    end
-
-    # Get terminal screen properties
-    #
-    # @see http://www.rubydoc.info/gems/tty-screen
-    #
-    # @api public
-    def screen
-      require 'tty-screen'
-      TTY::Screen
-    end
-
-    # The unix which utility
-    #
-    # @see http://www.rubydoc.info/gems/tty-which
-    #
-    # @api public
-    def which(*args)
-      require 'tty-which'
-      TTY::Which.which(*args)
-    end
-
-    # Check if executable exists
-    #
-    # @see http://www.rubydoc.info/gems/tty-which
-    #
-    # @api public
-    def exec_exist?(*args)
-      require 'tty-which'
-      TTY::Which.exist?(*args)
-    end
+    # def prompt(**options)
+    #   require "tty-prompt"
+    #   TTY::Prompt.new(options)
+    # end
   end
 end
     EOS
